136269840b9a78faff6c8c368195d2e5d3712bd8
Sebitas1201 <sebas1201@javerianacali.edu.co>
2025-08-21 23:04:43 -0500

    Primer commit

---
+++ untitled2/matematicas.h
@@ -0,0 +1,11 @@
+
+#ifndef MI_PROYECTO_MATEMATICAS_H
+#define MI_PROYECTO_MATEMATICAS_H
+
+
+// Declaraciones:
+int sumar(int a, int b);
+int restar(int a, int b);
+
+
+#endif //MI_PROYECTO_MATEMATICAS_H
